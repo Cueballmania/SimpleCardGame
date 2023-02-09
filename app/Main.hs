4d4360bada75010c97535990a72b8dfe4a6ca8e2
Brant Abeln <cuey@Brants-MacBook-Air.local>
2023-02-09 10:17:44 -0600

    StateT functions now return values. Refactored drawN to use Maybe

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Deck (Card, Deck, fullDeck, cardValue)
+import Deck (Card, Deck, shuffle, genDecks, cardValue)
 import System.Random
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Class
@@ -19,29 +19,26 @@ printTableValue :: [Card] -> IO ()
 printTableValue cs = putStrLn $ "The value of cards is " ++ show vals
     where vals = sum [cardValue c | c <- cs]
 
-shuffleDeck :: Monad m => GameT m ()
+shuffleDeck :: Monad m => GameT m Deck
 shuffleDeck = do
     game <- get
     let (shufDeck, gen') = shuffle (deck game) (gen game)
     let game' = game {deck = shufDeck, gen = gen'}
     put game'
+    return shufDeck
 
-shuffle :: Deck -> StdGen -> (Deck, StdGen)
-shuffle [] g = ([], g)
-shuffle dk g =
-    let (index, newGen) = randomR (0, length dk - 1) g
-        card = dk !! index
-        rest = take index dk ++ drop (index + 1) dk
-    in (card : fst (shuffle rest newGen), newGen)
-
-drawN :: Monad m => Int -> GameT m ()
+drawN :: Monad m => Int -> GameT m (Maybe [Card])
 drawN n = do
     g <- get
     let deck' = deck g
     let tabled = table g
     let discarded = discard g
-    let (drawnCards, remainingDeck) = splitAt n deck'
-    put $ g { deck = remainingDeck, table = drawnCards, discard = tabled ++ discarded}
+    if n > length deck'
+        then return Nothing
+        else do
+            let (drawnCards, remainingDeck) = splitAt n deck'
+            put $ g { deck = remainingDeck, table = drawnCards, discard = tabled ++ discarded}
+            return $ Just drawnCards
 
 clearTable :: Monad m => GameT m ()
 clearTable = do
@@ -62,21 +59,16 @@ gameLoop = do
                 lift $ putStrLn "Invalid input. Please enter a number."
                 gameLoop
         Just draw -> do
-            let remaining = length $ deck g
-            if draw > remaining
-                then lift $ putStrLn $ "Not enough cards left to draw " ++ 
+            cards <- drawN draw
+            case cards of
+                Nothing -> do
+                    let remaining = length $ deck g
+                    lift $ putStrLn $ "Not enough cards left to draw " ++ 
                             show draw ++ ". Only " ++ show remaining ++ " cards remaining."
-                else do
-                    drawN draw
-                    printTable
+                Just xs -> do
+                    lift $ putStrLn $ "Table: " ++ show xs
+                    lift $ printTableValue xs
                     gameLoop
-
-printTable :: GameT IO ()
-printTable = do
-    g <- get
-    let tabled = table g
-    lift $ putStrLn $ "Table: " ++ show tabled
-    lift $ printTableValue tabled
 
 main :: IO ()
 main = do
@@ -84,6 +76,6 @@ main = do
     seedStr <- getLine
     let seed = read seedStr :: Int
     let initGen = mkStdGen seed
-    let initialGame = Game {deck = fullDeck, discard = [], table = [], gen = initGen}
+    let initialGame = Game {deck = genDecks 6, discard = [], table = [], gen = initGen}
     _ <- execStateT (shuffleDeck >> gameLoop) initialGame
     putStrLn "Thanks for playing"
--- src/Deck.hs
+++ src/Deck.hs
@@ -1,5 +1,7 @@
 module Deck where
 
+import System.Random
+
 data Face = Ace | Two | Three | Four | Five | Six |
             Seven | Eight | Nine | Ten | Jack | Queen | King
             deriving (Show, Eq, Enum)
@@ -14,6 +16,18 @@ type Deck = [Card]
 
 fullDeck :: [Card]
 fullDeck = [Card face suit | suit <- [Clubs .. Spades], face <- [Ace .. King]]
+
+genDecks :: Int -> [Card]
+genDecks n = concat $ replicate n fullDeck
+
+shuffle :: Deck -> StdGen -> (Deck, StdGen)
+shuffle [] g = ([], g)
+shuffle dk g =
+    let (index, newGen) = randomR (0, length dk - 1) g
+        card = dk !! index
+        rest = take index dk ++ drop (index + 1) dk
+    in (card : fst (shuffle rest newGen), newGen)
+
 
 cardValue :: Card -> Int
 cardValue (Card face _) =
